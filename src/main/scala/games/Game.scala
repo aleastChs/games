c622974f9b049e21a88a10512872c1c65c151e82
aleastChs <aleast@student.chalmers.se>
2017-06-19 13:18:39 +0200

    Chnged gitignore

--- src/main/scala/games/Game.scala
+++ src/main/scala/games/Game.scala
@@ -1,8 +1,11 @@
 package games
+import scala.scalajs
 import scalajs.js
-import scala.scalajs.js.annotation.JSExport
+import js.annotation.JSExport
 import org.scalajs.dom
 import dom.html
+import scalatags.JsDom
+import JsDom.all._
 
 import scala.util.Random
 
@@ -11,6 +14,28 @@ import scala.util.Random
 object Game extends{
   @JSExport
   def main(target: html.Div) = {
-    
+    val box = input(
+      `type`:="text",
+      placeholder:="Type here!"
+    ).render
+
+    val output = span.render
+
+    box.onkeyup = (e: dom.Event) => {
+      output.textContent =
+        box.value.toUpperCase
+    }
+
+    target.appendChild(
+      div(
+        h1("Capital Box!"),
+        p(
+          "Type here and " +
+            "have it capitalized!"
+        ),
+        div(box),
+        div(output)
+      ).render
+    )
   }
 }
